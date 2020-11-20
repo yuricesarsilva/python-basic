e65e6b5f3c4ae2a4a19de0f88b1dd39e26717d45
Yuri Cesar de Lima e Silva <yuricesar15silva@gmail.com>
2020-11-19 23:13:28 -0400

    importação de código

---
+++ fundamentos.py
@@ -0,0 +1,25 @@
+# Aula 1 - Primeiros exemplos:
+# Lembrar sempre de salvar antes de rodar o programa (ctrl+S)
+
+print('primeiro programa')
+
+# Use a \ para quebrar a linha (python se preocupa com a quebra das linhas)
+1 \
+    + 2
+
+help(print)
+
+'''------------------------------------------------------------'''
+#Aula 2 - Estruturas de dados (Tipos básicos de dados):
+print(True)
+print(False)
+print(1.2 + 1)
+print('Aqui eu falo a minha lingua!')
+print("também funciona.")
+print('Você é ' + 3 * 'muito ' + 'legal!')
+# print(3+ '3') -> ambiguidade
+print([1, 2, 3])
+print({'nome': 'pedro', 'idade':22})
+print(None)
+
+
